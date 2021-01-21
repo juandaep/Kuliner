a0ebbc046a6743ebd37b35b3659ed9abe885ca8a
juandaep <36181732+juandaep@users.noreply.github.com>
2021-01-22 01:53:04 +0700

    initial commit

---
+++ src/components/Hero.vue
@@ -0,0 +1,28 @@
+<template>
+  <div class="row mt-5">
+    <div class="col">
+      <div class="d-flex h-100">
+        <div class="justify-content-center align-self-center">
+          <h2>
+            <strong>Delicious Food Menu,</strong>
+            <br />in Your Gadget
+          </h2>
+          <p>Pilih dan pesan makanan favorit anda</p>
+          <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right> Pesan</button>
+        </div>
+      </div>
+    </div>
+    <div class="col">
+      <img src="../assets/images/hero.png" alt="hero" width="100%" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Hero",
+};
+</script>
+
+<style>
+</style>
