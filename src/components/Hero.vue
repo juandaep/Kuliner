699ff1bff32ec1d18fb6b3bdc4fd4ab19a9ddba2
Juanda Esa <juandaesaputra@gmail.com>
2021-01-22 16:01:47 +0700

    Add Json server

--- src/components/Hero.vue
+++ src/components/Hero.vue
@@ -1,19 +1,49 @@
 <template>
-  <div class="row mt-5">
-    <div class="col">
-      <div class="d-flex h-100">
-        <div class="justify-content-center align-self-center">
-          <h2>
-            <strong>Delicious Food Menu,</strong>
-            <br />in Your Gadget
-          </h2>
-          <p>Pilih dan pesan makanan favorit anda</p>
-          <button class="btn btn-lg btn-success"><b-icon-arrow-right></b-icon-arrow-right> Pesan</button>
+  <div class="hero">
+    <!-- Desktop -->
+    <div class="mt-5 d-none d-md-block ">
+      <div class="row">
+      <div class="col-md-6">
+        <div class="d-flex h-100">
+          <div class="justify-content-center align-self-center">
+            <h2>
+              <strong>Delicious Food Menu,</strong>
+              <br />in Your Gadget
+            </h2>
+            <p>Pilih dan pesan makanan favorit anda</p>
+            <button class="btn btn-lg btn-success">
+              <b-icon-arrow-right></b-icon-arrow-right> Pesan
+            </button>
+          </div>
+        </div>
+      </div>
+      <div class="col-md-6">
+        <img src="../assets/images/hero.png" alt="hero" width="100%" />
+      </div>
+    </div>
+    </div>
+
+    <!-- Mobile -->
+    <div class="d-sm-block d-md-none">
+      <div class="row mt-5">
+      <div class="col-md-6 mb-3">
+        <img src="../assets/images/hero.png" alt="hero" width="100%" />
+      </div>
+      <div class="col-md-6">
+        <div class="d-flex h-100">
+          <div class="justify-content-center align-self-center">
+            <h2>
+              <strong>Delicious Food Menu,</strong>
+              <br />in Your Gadget
+            </h2>
+            <p>Pilih dan pesan makanan favorit anda</p>
+            <button class="btn btn-lg btn-success">
+              <b-icon-arrow-right></b-icon-arrow-right> Pesan
+            </button>
+          </div>
         </div>
       </div>
     </div>
-    <div class="col">
-      <img src="../assets/images/hero.png" alt="hero" width="100%" />
     </div>
   </div>
 </template>
